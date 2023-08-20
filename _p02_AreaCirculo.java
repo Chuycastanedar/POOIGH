4317ba3e32c8316a09d056bdb5d8ff922816749e
Chuycastanedar <chuycastaneda_@outlook.com>
2023-08-20 11:37:43 -0600

    Repositorio

--- _p02_AreaCirculo.java
+++ _p02_AreaCirculo.java
@@ -1,18 +1,18 @@
 // Calcula el área de un círculo
 
+import java.util.Scanner;
+
 public class _p02_AreaCirculo {
     public static void main(String[] args) {
         double radio, area;
-        Scanner lradio = new Scanner System.in;
+        Scanner lradio = new Scanner(System.in);
 
-        System.out.println"\nCalculando el área de un círculo\n";
-        System.out.print"Dame el radio: ";
-        radio = lradio.nextFloat ;
+        System.out.println("\nCalculando el área de un círculo\n");
+        System.out.print("Dame el radio: ");
+        radio = lradio.nextFloat();
 
-        area = Math.PI = Math.pow radio, 2 ;
+        area = Math.PI * Math.pow(radio, 2);
 
-        System.out.println"El circulo de radio " + radio + "tiene área de " + area ;
-
-
+        System.out.println("El circulo de radio " + radio + " tiene área de: " + area);
     }
 }
--- _p03_AreaTriangulo.java
+++ _p03_AreaTriangulo.java
@@ -1,1 +1,21 @@
 // Calcula el área de un triángulo a partir de sus datos
+
+import java.util.Scanner;
+
+public class _p03_AreaTriangulo {
+    public static void main(String[] args) {
+        
+    int base, altura;
+    double area;
+    Scanner obj = new Scanner(System.in);
+
+    System.out.print("Ingresa la base: ");
+    base = obj.nextInt();
+    System.out.print("Ingresa la altura: ");
+    altura = obj.nextInt();
+
+    area = base + altura /2;
+    System.out.println(String.format("El triángulo de base: %d y altura: %d tiene un área de: %f", base, altura, area));
+
+    }
+}
