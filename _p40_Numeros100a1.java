7c0a3e30ad0d72d48c41d5c6c96e323277df45d6
Chuycastanedar <chuycastaneda_@outlook.com>
2023-09-07 02:15:41 -0600

    Actividad 7

--- _p40_Numeros100a1.java
+++ _p40_Numeros100a1.java
@@ -2,12 +2,12 @@
 
 public class _p40_Numeros100a1 {
     public static void main(String[] args) {
-    int c;
-    c=100;
-    while( c >= 1 ) {
-        System.out.print(c+" ");
-        c--;
-    }
-    System.out.println("\nProceso terminado ...");
+        int c;
+        c=100;
+        while( c >= 1 ) {
+            System.out.print(c+" ");
+            c--;
+        }
+        System.out.println("\nProceso terminado ...");
 }
 }
